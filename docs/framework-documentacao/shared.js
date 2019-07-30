24412925a5e1a53bc42c231aff3a0c3550352efa
rsg73626 <renan.rsg@hotmail.com>
2019-07-29 21:38:24 -0300

    Criando arquivos de documentação.

--- docs/framework-documentacao/shared.js
+++ docs/framework-documentacao/shared.js
@@ -1,11 +1,12 @@
 
 function getHeaderObjects() {
 
-    const home = mi('Inicio', '/index.html')
-    const about = mi('Sobre', '/about/about.html')
-    const accessibility = mi('Acessibilidade', '/accessibility/accessibility.html')
-    const getstarted = mi('Como usar?', '/getstarted/getstarted.html')
+    const home = mi('Inicio', '/docs/framework-documentacao/index.html')
+    const about = mi('Sobre', '/docs/framework-documentacao/about/about.html')
+    const accessibility = mi('Acessibilidade', '/docs/framework-documentacao/accessibility/accessibility.html')
+    const getstarted = mi('Como usar?', '/docs/framework-documentacao/getstarted/getstarted.html')
     const mainMenu = mn([home, about, accessibility, getstarted])
+    mainMenu.size = menu_large
 
     return [mainMenu]
 
